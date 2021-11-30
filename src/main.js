47d3f5ee76686a430ba7f0420e3e8a87b3c5e5d1
Grigoriy_Belikov <grisha.belikov@gmail.com>
2021-11-30 16:56:39 +0300

    - Добавлены компоненты

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {renderTemplate, RenderPosition} from './render.js';
-// import {createShowMoreButtonTemplate} from './view/show-more-button-view.js';
+import {createShowMoreButtonTemplate} from './view/show-more-button-view.js';
 // import {createMoreDetailsTemplate} from './view/more-details-view.js';
 import {createFilmCardTemplate} from './view/movie-card-view.js';
 import {createSiteMenuTemplate, createMovieStatisticTemplate} from './view/site-menu-view.js';
@@ -7,16 +7,13 @@ import {createUserRankTemplate} from './view/user-rank-view.js';
 
 const siteMainElement = document.querySelector('.main');
 renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createFilmCardTemplate(), RenderPosition.BEFOREEND);
 
-for (let i = 0; i < 5; i++) {
-  renderTemplate(siteMainElement, createFilmCardTemplate(), RenderPosition.BEFOREEND);
-}
+renderTemplate(siteMainElement, createShowMoreButtonTemplate(), RenderPosition.BEFOREEND);
 
 const siteHeaderElement = document.querySelector('.header');
-const siteHeaderLogo = siteHeaderElement.querySelector('.header__logo.logo');
-renderTemplate(siteHeaderLogo, createUserRankTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteHeaderElement, createUserRankTemplate(), RenderPosition.BEFOREEND);
 
 const siteFooterElement = document.querySelector('.footer');
 const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 renderTemplate(footerStatisticsElement, createMovieStatisticTemplate(), RenderPosition.BEFOREEND);
-
--- src/view/site-menu-view.js
+++ src/view/site-menu-view.js
@@ -17,8 +17,7 @@ export const createSiteMenuTemplate = () => (
 );
 
 export const createMovieStatisticTemplate = () => (
-  `<section class="footer__logo logo logo--smaller">Cinemaddict</section>
-  <section class="footer__statistics">
+  `
     <p>130 291 movies inside</p>
-  </section>`
+  `
 );
